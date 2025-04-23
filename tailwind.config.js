fb36c4ae80304e5fe7c8d58cc1cf37ca89e8d4e7
konsevdi <103928955+konsevdi@users.noreply.github.com>
2025-04-23 16:07:58 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'vibe-purple': '#7f00ff',
+        'vibe-pink': '#e100ff',
+      },
+      fontFamily: {
+        rethink: ['Rethink Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
